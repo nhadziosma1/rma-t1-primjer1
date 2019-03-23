096414484381e952144f9963d15bd16bc8393ee7
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2019-03-23 12:29:31 +0100

    prvi push sa android studija

---
+++ app/src/main/java/ba/unsa/etf/rma/VJ_18033/MainActivity.java
@@ -0,0 +1,53 @@
+package ba.unsa.etf.rma.VJ_18033;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button dugmeVan;
+    private TextView tekstVan;
+    private ListView listViewVan;
+
+    private ArrayAdapter ulancaniAdapter;
+    private ArrayList<String> unosiKorisnika = new ArrayList<>();
+    private EditText editujMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //predlaze id-eve svih elemenata sa prozaora kao upises "R.id. "
+        dugmeVan = (Button) findViewById(R.id.dugmeVan);
+        //mora se izvrsiti cast u TexzView, jer ova metoda vraca "View" tip
+        tekstVan = (TextView) findViewById(R.id.TVVan);
+        listViewVan = (ListView) findViewById(R.id.listaPogled);
+        editujMe = (EditText) findViewById(R.id.editujMe);
+
+
+        ulancaniAdapter = new ArrayAdapter(getBaseContext(), android.R.layout.simple_list_item_1, unosiKorisnika);
+        listViewVan.setAdapter(ulancaniAdapter);
+
+        dugmeVan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                unosiKorisnika.add(0, editujMe.getText().toString());
+                //unosiKorisnika.add(0, tekstVan.getText().toString());
+                ulancaniAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
